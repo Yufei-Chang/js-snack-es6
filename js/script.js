5e3eb9eee112425d92aff318d79cd0ce9edb59d1
Yufei-Chang <yufei.chang@hotmail.com>
2024-11-12 15:59:11 +0100

    feat: esercizio1 fatto

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const myBikes = [
+    {nome: "bike1", peso: 10, colore: "verde"},
+    {nome: "bike2", peso: 7, colore: "rosso"},
+    {nome: "bike3", peso: 9, colore: "giallo"},
+    {nome: "bike4", peso: 10, colore: "blu"},
+    {nome: "bike5", peso: 4, colore: "nero"},
+]
+
+let lighterBike = myBikes[0];
+
+for (let i = 0; i < myBikes.length; i++) {
+    if (myBikes[i].peso < lighterBike.peso) {
+        lighterBike = myBikes [i];
+    }
+}
+
+console.log("La bici più leggera è:", lighterBike)
